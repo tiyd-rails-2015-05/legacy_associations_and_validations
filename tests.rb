a140a95bba2c4c0011fccfd63026261295c2d5e2
Danai Adkisson <dadkisso@live.com>
2015-05-20 18:43:03 -0400

    set up school with 2 terms and 2 courses per term

--- tests.rb
+++ tests.rb
@@ -28,38 +28,55 @@ class ApplicationTest < Minitest::Test
     assert true
   end
 
-#how do I test this????
+
   def test_term_can_not_be_destroyed_if_courses_present
-    term = Term.new(name: "Fall")
-    course = Course.new(name: "Marching Band")
+    fall_term = Term.create(name: "Fall")
+    course = Course.create(name: "Marching Band", term_id: fall_term.id)
 
-    assert term.save
-    assert course.save
-
-    term.destroy
-    refute term.destroy
+    refute fall_term.destroy
   end
 
-#how do I test this????
+
   def test_course_can_not_be_destroyed_if_course_students_present
-    course = Course.new(name: "Marching Band")
-    students = CourseStudent.new(student_id: 1)
+    band_course = Course.create(name: "Marching Band")
+    students = CourseStudent.create(student_id: 1, course_id: band_course.id)
 
-    assert course.save
-    assert students.save
-
-    course.destroy
-    refute course.destroy
-
-
+    band_course.destroy
   end
 
-#how do I test this????
-  def test_assignments_are_destroyed_when_courses_are_destroyed
-    course = Course.create(name: "Marching Band")
-    course = Assignment.create(name: "Malaguena")
-  end
+#how do I test this????=========================================================
+  # def test_assignments_are_destroyed_when_courses_are_destroyed
+  #   band_course = Course.create(name: "Marching Band")
+  #   assignment = Assignment.create(name: "Malaguena", course_id: band_course.id )
+  #
+  #
+  #   band_course.destroy
+  #   assert_equal band_course , assignment
+  # end
+  #  ============================================================================
+  # def test_lessons_and_pre_assign
+  #
+  # end
 
+
+  def test_school_set_up
+    uw = School.create(name: "University of Wyoming")
+    fall_term = Term.create(name: "Fall", school_id: uw.id)
+    band_course = Course.create(name: "Marching Band", course_code: "MUSC 3000", term_id: fall_term.id)
+    theory_course = Course.create(name: "Music Theory", course_code: "MUSC 1050", term_id: fall_term.id)
+
+    spring_term = Term.create(name: "Spring", school_id: uw.id)
+    jazz_course = Course.create(name: "Jazz Band", course_code: "MUSC 2500", term_id: spring_term.id)
+    symphony_course = Course.create(name: "Wind Symphony", course_code: "MUSC 4500", term_id: spring_term.id)
+
+    assert uw
+    assert fall_term
+    assert band_course
+    assert theory_course
+    assert spring_term
+    assert jazz_course
+    assert symphony_course
+  end
 
 
 
