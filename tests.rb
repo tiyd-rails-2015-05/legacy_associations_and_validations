9e4f22040ca79f833e84415b8b4fb37f986ff8da
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-20 17:41:29 -0400

    Pass term must have start date test

--- tests.rb
+++ tests.rb
@@ -34,10 +34,18 @@ class ApplicationTest < Minitest::Test
   end
 
   def test_terms_must_have_name
-    third = Term.new(name: "Third")
-    second = Term.new({})
+    third = Term.new(name: "Third", starts_on: 2015-01-05)
+    second = Term.new({starts_on: 2014-11-01})
 
     assert third.save
+    refute second.save
+  end
+
+  def test_terms_have_starts_on
+    fourth = Term.new(name: "Fourth", starts_on: 2015-04-05)
+    second = Term.new(name: "Second")
+
+    assert fourth.save
     refute second.save
   end
 
