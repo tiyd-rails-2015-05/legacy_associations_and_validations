97803b8ec895e882fcc4b6c216875477c66188bc
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-20 17:47:57 -0400

    Pass term has ends on test

--- tests.rb
+++ tests.rb
@@ -34,19 +34,26 @@ class ApplicationTest < Minitest::Test
   end
 
   def test_terms_must_have_name
-    third = Term.new(name: "Third", starts_on: 2015-01-05)
-    second = Term.new({starts_on: 2014-11-01})
+    third = Term.new(name: "Third", starts_on: 2015-01-05, ends_on: 2015-03-30)
+    second = Term.new({starts_on: 2014-11-01, ends_on: 2015-01-01})
 
     assert third.save
     refute second.save
   end
 
   def test_terms_have_starts_on
-    fourth = Term.new(name: "Fourth", starts_on: 2015-04-05)
-    second = Term.new(name: "Second")
+    fourth = Term.new(name: "Fourth", starts_on: 2015-04-05, ends_on: 2015-07-01 )
+    second = Term.new(name: "Second", ends_on: 2015-01-01)
 
     assert fourth.save
     refute second.save
   end
 
+  def test_terms_have_ends_on
+    fifth = Term.new(name: "Fifth", starts_on: 2015-04-05, ends_on: 2015-07-01)
+    second = Term.new(name: "Second", starts_on: 2014-10-11)
+
+    assert fifth.save
+    refute second.save
+  end
 end
